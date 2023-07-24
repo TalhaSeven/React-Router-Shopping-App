559170115afe16046cf59b114b0a5d09c41afb76
Talha Seven <131260032+TalhaSeven@users.noreply.github.com>
2023-07-24 17:54:02 +0300

    responsive done

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,23 +1,47 @@
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { Link, NavLink } from "react-router-dom";
 
 const NavbarComp = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavClose = () => {
+    setExpanded(false);
+  };
+
   return (
-   
-      <Navbar bg="dark" data-bs-theme="dark" className="nav">
-        <Container>
-          <Navbar.Brand as={Link} to="/" className="fs-3 shop">Seven's Shop</Navbar.Brand>
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="md"
+      expanded={expanded}
+      onToggle={() => setExpanded((prevExpanded) => !prevExpanded)}
+    >
+      <Container>
+        <Navbar.Brand as={Link} to="/" className="fs-3 shop">
+          Seven's Shop
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto navLink">
-            <Nav.Link as={NavLink} style={({isActive})=>({color:isActive && "red" })} to="/products">Products</Nav.Link>
-            <Nav.Link as={NavLink} style={({isActive})=>({color:isActive && "red" })} to="/campaigns">Campaigns</Nav.Link>
-            <Nav.Link as={NavLink} style={({isActive})=>({color:isActive && "red" })} to="/about">About</Nav.Link>
-            <Nav.Link as={NavLink} style={({isActive})=>({color:isActive && "red" })} to="/contact">Contact</Nav.Link>
+            <Nav.Link as={NavLink} to="/products" onClick={handleNavClose}>
+              Products
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/campaigns" onClick={handleNavClose}>
+              Campaigns
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/about" onClick={handleNavClose}>
+              About
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/contact" onClick={handleNavClose}>
+              Contact
+            </Nav.Link>
           </Nav>
-        </Container>
-      </Navbar>
-    
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
--- src/pages/Campaigns.jsx
+++ src/pages/Campaigns.jsx
@@ -4,20 +4,18 @@ import { Outlet, useNavigate } from "react-router-dom";
 const Campaigns = () => {
   const navigate = useNavigate();
   return (
-    <div className="nestedPage">
+    <div className="nestedPage gap-2 col-6 mx-auto">
       <h1>Campaigns</h1>
-      <div className="mt-3">
+      <div className="mt-">
         <button
           onClick={() => navigate("blackfriday")}
-          className="btn btn-success me-2 px-5"
-        >
-          BlackFriday Campaign
+          className="btn btn-outline-success px-5"
+        >BlackFriday Campa
         </button>
         <button
           onClick={() => navigate("discounts")}
-          className="btn btn-warning px-5"
-        >
-          Suprise Discounts
+          className="btn btn-outline-success m-3 px-5"
+        >Suprise Discounts
         </button>
       </div>
       <Outlet />
